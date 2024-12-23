243580ae32099ee9396f3b3dc9e5ff9e0163b904
Shailesh <mallicksailesh957@gmail.com>
2024-12-23 20:41:37 +0530

    useFetch hook

---
+++ src/components/useFetch.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const useFetch =(url)=>{
+
+
+    //States
+    const[data,setData]=useState('')
+    const[loading,setLoading]=useState(true)
+
+    const fetchData = async(url)=>{
+        const res = axios.get(url)
+        setData(res.data)
+        setLoading(false)
+
+    }
+
+    useEffect(()=>{
+        fetchData(url)
+    },[url])
+
+    return(
+        {data,loading}
+    )
+
+
+}
+
+export default useFetch
