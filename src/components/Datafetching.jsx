56e0b499b3a07e518679e98f2d3fc13c1eb04bd9
Shailesh <mallicksailesh957@gmail.com>
2024-12-23 16:14:48 +0530

    Data fetching with loading state

--- src/components/Datafetching.jsx
+++ src/components/Datafetching.jsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 import axios from 'axios'
 const Datafetching = () => {
 
-    const [data,setData]= useState('');
+    const [data,setData]= useState(null);
+    const [loading,setLoading]=useState(true)
+//Handle the loading state
 
     const HandleData =async()=>{
         
         try{
 
-           const data = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-           if(data){
-            setData(data)
-           }
-        }catch(err){
+           const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+           setData(res.data)
+           setLoading(false)
+           
+        }
+        catch(err){
             console.log("Error in fetching the data")
         }
     }
@@ -22,8 +25,9 @@ const Datafetching = () => {
     <>
     <div>
         <h1>Data fetching ....</h1>
+        {loading && <h1>The data is loading please have patience</h1>}
         <button onClick={HandleData}> Fetch Dummy Data </button>
-        <h1>The fetched data title is :{data.title}</h1>
+        {data&&<h1>The fetched data title is :{data.title}</h1>}
     </div>
     </>
   )
